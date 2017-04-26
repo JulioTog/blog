f1e3089e130882a3f691a672d0e8d7410fe05718
JulioTog <julio.tognetti@gmail.com>
2017-04-26 01:19:06 -0300

    Part24
    -Added route blog/{slug}
    -New controller BlogController
    -New view single

--- resources/views/Pages/welcome.blade.php
+++ resources/views/Pages/welcome.blade.php
@@ -18,7 +18,7 @@
           <div class="text">
                 <h3> {{ $post->title }}</h3>
                 <p> {{ $post->body }}</p>
-                <a href="#"class="btn btn-primary">Read more</a>
+                <a href="{{url('blog/'.$post->slug)}}"class="btn btn-primary">Read more</a>
           </div>
           <hr>
         @endforeach
